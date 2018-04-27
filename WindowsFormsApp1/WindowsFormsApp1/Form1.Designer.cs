4a44f382fb15c834aca037938028aa7f242958e7
JAVIER ANTON <javidoc@v240jam.jazztel.es>
2018-04-27 22:33:32 +0200

    SE HA AÑADIDO UNA CAJA DE LISTA

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -29,11 +29,12 @@
         private void InitializeComponent()
         {
             this.bMensaje = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // bMensaje
             // 
-            this.bMensaje.Location = new System.Drawing.Point(236, 95);
+            this.bMensaje.Location = new System.Drawing.Point(32, 28);
             this.bMensaje.Name = "bMensaje";
             this.bMensaje.Size = new System.Drawing.Size(141, 63);
             this.bMensaje.TabIndex = 0;
@@ -41,11 +42,26 @@
             this.bMensaje.UseVisualStyleBackColor = true;
             this.bMensaje.Click += new System.EventHandler(this.bMensaje_Click);
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "PRIMERA",
+            "SEGUNDA",
+            "TERCERA",
+            "CUARTA",
+            "QUINTA"});
+            this.listBox1.Location = new System.Drawing.Point(221, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(179, 160);
+            this.listBox1.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(644, 450);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.bMensaje);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +72,7 @@
         #endregion
 
         private System.Windows.Forms.Button bMensaje;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
